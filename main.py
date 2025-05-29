d879dc82d1538f40765ef7e8e7fe23345a8ede92
sudonetizen <sardortolibjonov1039@gmail.com>
2025-05-29 18:42:20 +0500

    cell

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-from w import Window
+from w import Window, Point, Line, Cell
 
-win = Window(800, 600)
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+    
+    #point1 = Point(0, 0)
+    #point2 = Point(200, 200)
+    #line = Line(point1, point2)
+    #win.draw_line(line, "green")
+    
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
+    win.wait_for_close()
+
+main()
--- w.py
+++ w.py
@@ -4,14 +4,17 @@ class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("maze solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", width=width, height=height)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
+
+    def draw_line(self, line_obj, color="black"):
+        line_obj.draw(self.canvas, color)
 
     def wait_for_close(self):
         self.running = True
@@ -21,4 +24,74 @@ class Window:
     def close(self):
         self.running = False
           
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1 
+        self.point2 = point2
+
+    def draw(self, canvas, color="black"):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=color, width=2)
+        
+class Cell:
+    def __init__(self, window_obj):
+        self.has_left_wall = True 
+        self.has_right_wall = True 
+        self.has_top_wall = True 
+        self.has_bottom_wall = True 
+
+        self.x1 = -1
+        self.x2 = -1
+        self.y1 = -1
+        self.y2 = -1
+
+        self.win = window_obj
+
+    def draw(self, x1, y1, x2, y2):
+        if self.win is None: return 
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+        if self.has_left_wall:
+           point1 = Point(x1, y1)
+           point2 = Point(x1, y2)
+           line = Line(point1, point2)
+           self.win.draw_line(line) 
+        if self.has_top_wall:
+           point1 = Point(x1, y1)
+           point2 = Point(x2, y1)
+           line = Line(point1, point2)
+           self.win.draw_line(line) 
+        if self.has_right_wall:
+           point1 = Point(x2, y1)
+           point2 = Point(x2, y2)
+           line = Line(point1, point2)
+           self.win.draw_line(line) 
+        if self.has_bottom_wall:
+           point1 = Point(x1, y2)
+           point2 = Point(x2, y2)
+           line = Line(point1, point2)
+           self.win.draw_line(line) 
+
+    def draw_move(self, to_cell, undo=False):
+        half = abs(self.x2 - self.x1) // 2
+        xc = half + self.x1
+        yc = half + self.y1
+
+        half2 = abs(to_cell.x2 - to_cell.x1) // 2
+        xc2 = half2 + to_cell.x1
+        yc2 = half2 + to_cell.y1
+
+        color = "red"
+        if undo == True: color = "gray"
+
+        point1 = Point(xc, yc) 
+        point2 = Point(xc2, yc2)
+        line = Line(point1, point2)
+        self.win.draw_line(line, color)
